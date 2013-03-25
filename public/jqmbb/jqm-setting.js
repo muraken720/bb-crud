66a0070ca01220f3117906d49efca7c467347f10
muraken720 <kenichiro.murata@gmail.com>
2013-03-26 03:31:26 +0900

    コードを一部修正

--- public/jqmbb/jqm-setting.js
+++ public/jqmbb/jqm-setting.js
@@ -1,6 +1,6 @@
 $(document).on("mobileinit", function () {
   "use strict";
+  $.mobile.page.prototype.options.addBackBtn = true;
   $.mobile.linkBindingEnabled = false;
   $.mobile.hashListeningEnabled = false;
-
 });
